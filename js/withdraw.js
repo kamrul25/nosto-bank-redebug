5547bef0fb78ec1a7f52cfaf804c501567fd61ac
kamrul25 <kamrul.coder01@gmail.com>
2023-03-10 22:56:39 +0600

    re debug nosto bank

--- js/withdraw.js
+++ js/withdraw.js
@@ -11,10 +11,15 @@
 
 document.getElementById("btn-withdraw").addEventListener("click", function () {
   const newWithdrawAmount = getInputFieldValueById("withdraw-field");
-  const previousWithdrawTotal = getTextElementValueById("withdraw-total ");
+  const previousWithdrawTotal = getTextElementValueById("withdraw-total");
   const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  setTextElementValueById(withdraw - total, newWithdrawTotal);
   const previousBalanceTotal = getTextElementValueById("balance-total");
-  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-  setTextElementValueById("balance-total", newBalanceTotal);
+ 
+  if(previousBalanceTotal >= newWithdrawTotal){
+    setTextElementValueById("withdraw-total", newWithdrawTotal);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    setTextElementValueById("balance-total", newBalanceTotal);
+  }else{
+    alert("Haram jada tor baper ar bank ato taka nai");
+  }
 });
